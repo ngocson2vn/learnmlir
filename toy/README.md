b257db68a9a6dde02e67cb72415e3399155dc893
Son Nguyen <son.nguyen@bytedance.com>
2024-06-17 07:02:46 +0000

    Update build script

--- toy/README.md
+++ toy/README.md
@@ -5,3 +5,13 @@ building a compiler for a simple Toy language.
 
 See [docs/Tutorials/Toy](../../docs/Tutorials/Toy) at the root of
 the project for more informations.
+
+## Build
+```Bash
+./build.sh TARGET_CHAPTER
+```
+
+For example:
+```Bash
+./build.sh Ch2
+```
--- toy/build.sh
+++ toy/build.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
 
+set -e
+
+target_chapter="$1"
+
+if ! [[ $target_chapter = @(Ch1|Ch2|Ch3|Ch4|Ch5|Ch6|Ch7) ]]; then
+  echo "Target \"$target_chapter\" is unsupported"
+  exit 1
+fi
+
 # CC=clang bazel build //Ch2:test
-CC=clang bazel build -s //Ch2:toyc --verbose_failures --sandbox_debug
+echo "Start building $target_chapter"
+CC=clang bazel build -s //$target_chapter:toyc --verbose_failures --sandbox_debug
