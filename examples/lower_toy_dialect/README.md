b8ae5c8faa7cc3e5c3b0c59976b596e17acfe079
Sony Nguyen <ngocson2vn@gmail.com>
2025-07-27 07:53:30 +0000

    Update examples/lower_toy_dialect

--- examples/lower_toy_dialect/README.md
+++ examples/lower_toy_dialect/README.md
@@ -13,10 +13,11 @@ sudo apt install lld-11
 sudo ln -sf /usr/lib/llvm-11/bin/clang /usr/bin/clang
 sudo ln -sf /usr/lib/llvm-11/bin/clang++ /usr/bin/clang++
 sudo ln -sf /usr/lib/llvm-11/bin/lld /usr/bin/lld
+sudo ln -sf /usr/lib/llvm-11/bin/ld.lld /usr/bin/ld.lld
 ```
 
 ## How to build
-### Step 1: Build llvm-project
+### Step 1: Clone llvm-project
 ```Bash
 cd learnmlir
 mkdir llvm-project
@@ -26,13 +27,13 @@ git remote add origin git@github.com:llvm/llvm-project.git
 git fetch origin --depth 1 fdac4c4e92e5a83ac5e4fa6d1d2970c0c4df8fa8
 git checkout FETCH_HEAD
 
-mkdir -v build
-cp -vf ../docs/build.sh build
-cd build
-./build.sh
+# mkdir -v build
+# cp -vf ../docs/build.sh build
+# cd build
+# ./build.sh
 
-# Verify
-find | grep -E '.*\.a$' | sort -V
+# # Verify
+# find | grep -E '.*\.a$' | sort -V
 ```
 <br/>
 
@@ -40,6 +41,7 @@ find | grep -E '.*\.a$' | sort -V
 ```Bash
 ./build.sh
 ```
+Both LLVM and MLIR libs will be built together.
 <br/>
 
 ## How to run
--- examples/lower_toy_dialect/build.sh
+++ examples/lower_toy_dialect/build.sh
@@ -4,57 +4,43 @@ set -e
 
 ROOT_DIR=$(pwd)
 echo "ROOT_DIR=${ROOT_DIR}"
+mkdir -p ${ROOT_DIR}/build
 
-# #========================================================
-# # Prerequisites
-# #========================================================
-# # llvm-project
-# llvm_hash=fdac4c4e92e5a83ac5e4fa6d1d2970c0c4df8fa8
-# echo "llvm_hash=${llvm_hash}"
-# mkdir -p llvm-project
-# cd llvm-project
-# git init
-# git remote add origin git@github.com:llvm/llvm-project.git
-# git fetch origin --depth 1 ${llvm_hash}
-# git checkout FETCH_HEAD
-# cd ..
-
-# rm -rf llvm-project/build
-# mkdir -v llvm-project/build
-# yes | cp -vf ../../docs/build.sh llvm-project/build
-
-# pushd llvm-project/build
-# ./build.sh
-# popd
+git submodule update --init --recursive
 
 
-# export CMAKE_PREFIX_PATH=${ROOT_DIR}/llvm-project/build/lib/cmake/:$CMAKE_PREFIX_PATH
-# export UBSAN_LIBRARY_DIR=${ROOT_DIR}/llvm-project/build/lib/clang/16.0.0/lib/x86_64-unknown-linux-gnu
+pre_hash=""
+if [ -f ./.cmake.sha256 ]; then
+  pre_hash=$(cat ./.cmake.sha256)
+fi
+now_hash=$(sha256sum ./CMakeLists.txt | awk '{print $1}')
 
-cd ${ROOT_DIR}/
-mkdir -p build
-cd build
-
-echo
-echo "==================================================="
-echo "Generate ninja build file"
-echo "==================================================="
-cmake -G Ninja .. \
-  -DCMAKE_BUILD_TYPE=Debug \
-  -DLLVM_ENABLE_PROJECTS="mlir;compiler-rt" \
-  -DLLVM_BUILD_EXAMPLES=ON \
-  -DLLVM_TARGETS_TO_BUILD="Native;X86;NVPTX;AMDGPU" \
-  -DLLVM_ENABLE_ASSERTIONS=ON \
-  -DCMAKE_C_COMPILER=clang \
-  -DCMAKE_CXX_COMPILER=clang++ \
-  -DLLVM_ENABLE_LLD=ON \
-  -DLLVM_CCACHE_BUILD=ON \
-  -DCOMPILER_RT_BUILD_GWP_ASAN=OFF \
-  -DLLVM_INCLUDE_TESTS=OFF \
-  -DCOMPILER_RT_BUILD_SANITIZERS=ON
+if [ "${now_hash}" != "${pre_hash}" ]; then
+  echo "${now_hash} != ${pre_hash}"
+  echo
+  echo "==================================================="
+  echo "Generate ninja build file"
+  echo "==================================================="
+  yes | echo ${now_hash} > ./.cmake.sha256
+  cd ${ROOT_DIR}/build
+  cmake -G Ninja .. \
+    -DCMAKE_BUILD_TYPE=Debug \
+    -DLLVM_ENABLE_PROJECTS="mlir;compiler-rt" \
+    -DLLVM_BUILD_EXAMPLES=OFF \
+    -DLLVM_TARGETS_TO_BUILD="Native;X86;NVPTX;AMDGPU" \
+    -DLLVM_ENABLE_ASSERTIONS=ON \
+    -DCMAKE_C_COMPILER=clang \
+    -DCMAKE_CXX_COMPILER=clang++ \
+    -DLLVM_ENABLE_LLD=ON \
+    -DLLVM_CCACHE_BUILD=ON \
+    -DCOMPILER_RT_BUILD_GWP_ASAN=OFF \
+    -DLLVM_INCLUDE_TESTS=OFF \
+    -DCOMPILER_RT_BUILD_SANITIZERS=ON
+fi
 
 echo
 echo "==================================================="
 echo "Run ninja build"
 echo "==================================================="
-cmake --build . -- -v
+cd ${ROOT_DIR}/build
+cmake --build .
